c511358769fbb04187c31e90ad2f7e936d4ea5ab
lucas campbell-rossen <lucas@rhomobile.com>
2012-04-25 16:22:48 -0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,11 +15,10 @@ QuotesService::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = false
-  config.assets.precompile += %w( public.js public.css  )
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
