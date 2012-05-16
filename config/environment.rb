8fd8424259a9d4346efeacba398e402d4dba92cc
lucas campbell-rossen <lucas@rhomobile.com>
2012-05-16 13:58:45 -0700

    new precompiled assets

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 require 'resque'
 require 'resque_scheduler'
-resque_file =  File.expand_path(File.join(File.dirname(__FILE__),'config','resque_schedule.yml'))
+resque_file =  File.expand_path(File.join(File.dirname(__FILE__),'resque_schedule.yml'))
 Resque.redis = "redis://redistogo:ec4a3f060076874728331f6378f6fd70@ray.redistogo.com:9067/"
 Resque.schedule = YAML.load_file(resque_file)
 # Initialize the rails application
