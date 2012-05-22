a178a97955530f620f31cea993ce8db93ded2d61
lucas campbell-rossen <lucas@rhomobile.com>
2012-05-21 19:50:06 -0700

    make prod test

--- config/configatron/production.rb
+++ config/configatron/production.rb
@@ -2,4 +2,7 @@
 # 
 # Example:
 #   configatron.file.storage = :s3
+ configatron.apn.passphrase  = ''
+ configatron.apn.port  = 2195
+ configatron.apn.host  = 'gateway.sandbox.push.apple.com'
 configatron.apn.cert = File.join(Rails.root,'config', 'apple_push_development.pem')
