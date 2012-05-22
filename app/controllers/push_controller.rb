e0563d445a0dbd52fc95a617e0f3c881caabcab7
lucas campbell-rossen <lucas@rhomobile.com>
2012-05-22 10:14:05 -0700

    limit alert size

--- app/controllers/push_controller.rb
+++ app/controllers/push_controller.rb
@@ -23,7 +23,7 @@ class PushController < ApplicationController
        badge = 5
        quote_id = params[:id]
        quote = Quote.find(quote_id.to_i)
-       alert = quote.quote[0..310]
+       alert = quote.quote[0..300]
        
        notification = APN::GroupNotification.new   
        notification.group = APN::Group.find_by_name("Apple")
