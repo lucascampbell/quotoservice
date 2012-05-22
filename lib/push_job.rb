ffbec3c91192447a29bdecf5b8c35229d7cde39e
lucas campbell-rossen <lucas@rhomobile.com>
2012-05-21 20:36:13 -0700

    make prod test

--- lib/push_job.rb
+++ lib/push_job.rb
@@ -6,6 +6,7 @@ class PushJob
     begin
       puts "about to send group notification"
       APN::App.send_group_notifications
+      puts "sent notification"
       #APN::App.process_devices
     rescue Exception => e
       puts "error #{e.message}"
