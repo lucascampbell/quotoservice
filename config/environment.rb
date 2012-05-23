4c6af78c2e7a8427aa51c49652bfdb3bcc52b1a6
lucas campbell-rossen <lucas@rhomobile.com>
2012-05-23 11:42:15 -0700

    what

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,14 @@ require 'resque'
 require 'resque_scheduler'
 require 'resque_scheduler/server'
 require 'apn_on_rails'
+require 'uri'
 
 resque_file =  File.expand_path(File.join(File.dirname(__FILE__),'resque_schedule.yml'))
-Resque.redis = "redis://redistogo:ef7b93c9efa962a832a49c8435de7b95@scat.redistogo.com:9034/"
+ENV["REDISTOGO_URL"] ||= "redis://redistogo:ef7b93c9efa962a832a49c8435de7b95@scat.redistogo.com:9034/"
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
 Resque.schedule = YAML.load_file(resque_file)
 # Initialize the rails application
 QuotesService::Application.initialize!
