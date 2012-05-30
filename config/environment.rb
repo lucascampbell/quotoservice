174415baa0d635fde8ef87c0a8375790ea4067a6
lucas campbell-rossen <lucas@rhomobile.com>
2012-05-29 19:04:09 -0700

    test

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,10 @@ require 'uri'
 
 resque_file =  File.expand_path(File.join(File.dirname(__FILE__),'resque_schedule.yml'))
 ENV["REDISTOGO_URL"] ||= "redis://redistogo:ef7b93c9efa962a832a49c8435de7b95@scat.redistogo.com:9034"
-Resque.redis = "redis://redistogo:ef7b93c9efa962a832a49c8435de7b95@scat.redistogo.com:9034"
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = REDIS
 
 Resque.schedule = YAML.load_file(resque_file)
 # Initialize the rails application
