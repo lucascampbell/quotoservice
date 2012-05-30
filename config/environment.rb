9b663207240ae50d925dc459789e4165554f8813
lucas campbell-rossen <lucas@rhomobile.com>
2012-05-29 18:56:51 -0700

    test

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ require 'c2dm_on_rails'
 require 'uri'
 
 resque_file =  File.expand_path(File.join(File.dirname(__FILE__),'resque_schedule.yml'))
-ENV["REDISTOGO_URL"] ||= "redis://redistogo:ef7b93c9efa962a832a49c8435de7b95@scat.redistogo.com:9034/"
-Resque.redis = "redis://redistogo:ef7b93c9efa962a832a49c8435de7b95@scat.redistogo.com:9034/"
+ENV["REDISTOGO_URL"] ||= "redis://redistogo:ef7b93c9efa962a832a49c8435de7b95@scat.redistogo.com:9034"
+Resque.redis = "redis://redistogo:ef7b93c9efa962a832a49c8435de7b95@scat.redistogo.com:9034"
 
 Resque.schedule = YAML.load_file(resque_file)
 # Initialize the rails application
