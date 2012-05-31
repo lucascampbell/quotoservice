511910faff416b4749313a94bef555f38f3703b7
lucas campbell-rossen <lucas@rhomobile.com>
2012-05-31 11:30:01 -0700

    new gem

--- config/configatron/production.rb
+++ config/configatron/production.rb
@@ -10,5 +10,5 @@
  
  configatron.c2dm.api_url = 'https://android.apis.google.com/c2dm/send'
  configatron.c2dm.username = 'quotesapp.tmc@gmail.com'
- configatron.c2dm.password = 'Oples10password'
+ configatron.c2dm.password = 'oops'
  configatron.c2dm.app_name = 'com.quotes'
