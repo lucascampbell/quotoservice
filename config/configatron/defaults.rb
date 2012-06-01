84873b07ecf09896fd980143129b56d8a2d80e76
lucas campbell-rossen <lucas@rhomobile.com>
2012-05-31 19:07:58 -0700

    new gem

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -7,7 +7,7 @@
 #   configatron.file.storage = :s3
 configatron.apn.cert = File.join('config', 'apple_push_development.pem')
 
-configatron.c2dm.api_url = 'https://android.apis.google.com/c2dm/send'
- configatron.c2dm.username = 'quotesapp.tmc@gmail.com'
- configatron.c2dm.password = 'Oples10password'
- configatron.c2dm.app_name = 'GoVerse'
+# configatron.c2dm.api_url = 'https://android.apis.google.com/c2dm/send'
+#  configatron.c2dm.username = 'quotesapp.tmc@gmail.com'
+#  configatron.c2dm.password = 'Oples10password'
+#  configatron.c2dm.app_name = 'GoVerse'
--- config/configatron/production.rb
+++ config/configatron/production.rb
@@ -9,6 +9,6 @@
  
  
  configatron.c2dm.api_url = 'https://android.apis.google.com/c2dm/send'
- configatron.c2dm.username = 'quotesapp.tmc@gmail.com'
- configatron.c2dm.password = 'Oples10password'
- configatron.c2dm.app_name = 'com.quotes'
+ configatron.c2dm.username = 'lkecamp1@gmail.com'
+ configatron.c2dm.password = 'hrtmember'
+ configatron.c2dm.app_name = 'goverse'
