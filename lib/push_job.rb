88464cccfc779a150aaf2cc57bcf8ebbd0057672
lucas campbell-rossen <lucas@rhomobile.com>
2012-06-02 11:06:39 -0700

    new gem

--- lib/push_job.rb
+++ lib/push_job.rb
@@ -5,8 +5,11 @@ class PushJob
 
   def self.perform()
     begin
+      #send for iphone
       app   = APN::App.first
       app.send_daily_apple_group_notification
+      #send for android
+      C2dm::Notification.send_daily_notification
       #APN::App.process_devices
     rescue Exception => e
       puts "error #{e.message}"
