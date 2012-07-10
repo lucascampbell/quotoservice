c143c28f08169cd01937110394ad8332445dc365
lucas campbell-rossen <lucas@rhomobile.com>
2012-07-10 14:10:37 -0700

    clean devices on job

--- lib/push_job.rb
+++ lib/push_job.rb
@@ -7,10 +7,11 @@ class PushJob
     begin
       #send for iphone
       app   = APN::App.first
+      app.process_devices
       app.send_daily_apple_group_notification
       #send for android
-      C2dm::Notification.send_daily_notification
-      app.process_devices
+      #C2dm::Notification.send_daily_notification
+      
     rescue Exception => e
       puts "error #{e.message}"
       raise e
