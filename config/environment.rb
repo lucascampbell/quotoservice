01e697fac5ef1259f4082a7c3e8c705339f2926d
lucas campbell-rossen <lucas@rhomobile.com>
2012-11-29 15:40:10 -0800

    new hirefire

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@ require 'resque_scheduler/server'
 require 'apn_on_rails'
 require 'c2dm_on_rails'
 require 'uri'
+require 'hirefireapp'
 
 resque_file =  File.expand_path(File.join(File.dirname(__FILE__),'resque_schedule.yml'))
 ENV["REDISTOGO_URL"] ||= "redis://redistogo:ef7b93c9efa962a832a49c8435de7b95@scat.redistogo.com:9034"
