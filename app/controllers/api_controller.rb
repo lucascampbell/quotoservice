55e51fe626c81168ec37e2740f66c1a4cb0449a2
lucas campbell-rossen <lucas@rhomobile.com>
2013-03-29 13:55:53 -0700

    fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,9 @@
 class ApiController < ApplicationController
   skip_before_filter :authenticate_user!
-  before_filter :authenticate_token!
+  #before_filter :authenticate_token! unless ['quotes_by_page','quotes_by_topic_id_name','topic_by_id_name','topics_by_status','topics_by_page'].include?(controller.action_name)
+  before_filter do |controller|
+       controller.send :authenticate_token! unless ['quotes_by_page','quotes_by_topic_id_name','topic_by_id_name','topics_by_status','topics_by_page'].include?(controller.action_name)
+  end
   
   def get_quotes
     #render :json => {:q =>"noupdates",:last_id => 1} and return
