390e279fa86cf88a3905f361487e47ecfeb95084
lucas campbell-rossen <lucas@rhomobile.com>
2013-05-03 11:49:44 -0700

    desc

--- app/mailers/submission_mailer.rb
+++ app/mailers/submission_mailer.rb
@@ -2,6 +2,6 @@ class SubmissionMailer < ActionMailer::Base
   default from: "app@goverse.org"
   
   def image_submission(email)
-    mail(:to=>email,:subject=>'Goverse Image Submission',:date=> Time.now,:content_type=>"text/html")
+    mail(:to=>email,:subject=>'GoVerse Contribution',:date=> Time.now,:content_type=>"text/html")
   end
 end
